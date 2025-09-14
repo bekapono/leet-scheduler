7e19951489b07264bba37fbd6e3f64dcc3ba51bd
bekapono <bsiliezar2@gmail.com>
2025-09-14 14:59:10 -0700

    feature - implemented problem repo and problem entity

--- app/entities/problem_entity.py
+++ app/entities/problem_entity.py
@@ -2,20 +2,18 @@ from datetime import datetime
 from sqlalchemy.orm.attributes import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import String, Enum, Integer, DateTime, Date
+from ..domain.difficulty_level import DifficultyLevel
 
 from Base_entity import Base
 
 
-class ProblemEntity(Base):
+class Problem(Base):
     __tablename__ = 'problems'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    difficulty: Mapped[Enum] = mapped_column(
-        Enum("EASY", "MEDIUM", "HARD", name="difficulty_level"),
-        nullable=False
-    )
+    difficulty: Mapped[DifficultyLevel] = mapped_column(Enum(DifficultyLevel), nullable=False)
     created_on: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False)
     updated_on: Mapped[datetime] = mapped_column(DateTime, onupdate=datetime.now(), nullable=False)
 
     def __repr__(self) -> str:
-        return f"ProblemEntity(id:{self.id}, title={self.title}, difficulty={self.difficulty})"
+        return f"Problem(id:{self.id}, title={self.title}, difficulty={self.difficulty})"
