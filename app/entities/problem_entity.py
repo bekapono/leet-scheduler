a5d933f3da37a2ad36f30ee5907d7851a3e930be
bekapono <bsiliezar2@gmail.com>
2025-09-18 11:42:04 -0700

    tidy - fixed import for difficulty_level

--- app/entities/problem_entity.py
+++ app/entities/problem_entity.py
@@ -2,15 +2,15 @@ from datetime import datetime
 from sqlalchemy.orm.attributes import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import String, Enum, Integer, DateTime, Date
-from ..domain.difficulty_level import DifficultyLevel
 
-from Base_entity import Base
+from app.entities.Base_entity import Base
+from app.domain.difficulty_level import DifficultyLevel
 
 
 class Problem(Base):
     __tablename__ = 'problems'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     difficulty: Mapped[DifficultyLevel] = mapped_column(Enum(DifficultyLevel), nullable=False)
     created_on: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False)
     updated_on: Mapped[datetime] = mapped_column(DateTime, onupdate=datetime.now(), nullable=False)
