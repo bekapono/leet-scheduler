f063084016a4104d58415b366093b52fd1327f98
bekapono <bsiliezar2@gmail.com>
2025-09-18 11:45:31 -0700

    init dependency push

---
+++ app/dependencies.py
@@ -0,0 +1,13 @@
+"""
+    Need to implement the dependencies for router
+"""
+
+
+class ProblemServiceDependency:
+    """ This class should init ProblemService class with Repository """
+    pass
+
+
+class SessionRepositoryDependency:
+    """ This class should init ProblemRepository class with session """
+    pass
